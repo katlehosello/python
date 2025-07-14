bf2fec9fa39f640d969a8931cbf30472762a8e76
katlehosello <katlehosello55@gmail.com>
2025-07-14 12:24:16 +0000

    Add calculator and modules files

---
+++ modules.py
@@ -0,0 +1,5 @@
+import math_operations
+
+print(math_operations.add(5, 4))
+
+print(math_operations.subtract(5, 2))
---
+++ rectangle_calc/exception_handling.py
@@ -0,0 +1,9 @@
+
+try:
+    print(x)
+
+except NameError:
+    print("Variable x not defined")
+
+else:
+    print("Everything went wrong")
